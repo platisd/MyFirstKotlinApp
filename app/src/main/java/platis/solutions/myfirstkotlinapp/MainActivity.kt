6f542cd8b9338206d2bf11e72d8bdc553a89463b
Dimitris Platis <platisd@users.noreply.github.com>
2018-03-12 22:50:21 +0100

    Add initial Kotlin app

---
+++ app/src/main/java/platis/solutions/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package platis.solutions.myfirstkotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import com.github.kittinunf.result.getAs
+
+class MainActivity : AppCompatActivity() {
+
+    var ipField: TextView? = null
+    internal val SERVER_IP = "https://api.ipify.org"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.ip_button)
+        // It would also work like this:
+        // var button : Button = findViewById(R.id.ip_button)
+
+        ipField = findViewById(R.id.ip_field)
+        button.setOnClickListener {
+            ipField?.text = "You need to implement this"
+        }
+    }
+}
