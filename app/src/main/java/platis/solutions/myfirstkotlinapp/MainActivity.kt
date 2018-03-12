78fcdf54e6c70a0621da0758b07b2a721e47db23
Dimitris Platis <platisd@users.noreply.github.com>
2018-03-12 22:56:28 +0100

    Add GET request snippet

--- app/src/main/java/platis/solutions/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/platis/solutions/myfirstkotlinapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +22,18 @@ class MainActivity : AppCompatActivity() {
 
         ipField = findViewById(R.id.ip_field)
         button.setOnClickListener {
-            ipField?.text = "You need to implement this"
+            //an extension over string (support GET, PUT, POST, DELETE with httpGet(), httpPut(), httpPost(), httpDelete())
+            SERVER_IP.httpGet().responseString { request, response, result ->
+                //do something with response
+                when (result) {
+                    is Result.Failure -> {
+                        ipField?.text = "Error"
+                    }
+                    is Result.Success -> {
+                        ipField?.setText(result.value)
+                    }
+                }
+            }
         }
     }
 }
